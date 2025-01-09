87ec6831c67160862a8a03098555905dea1ccc4a
Filipp Sam <philippsm666@gmail.com>
2025-01-09 22:03:10 +0300

    (sprint 1 week 1) 3 points completed

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -17,7 +17,7 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = 'high' | 'low' | 'middle'// need to fix any
 export type AffairType = {
     _id: any // need to fix any
     name: any // need to fix any
@@ -26,7 +26,7 @@ export type AffairType = {
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: Array<AffairType> = [ // need to fix any
     {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -35,17 +35,19 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any
+    if (filter === 'all') {
+        return affairs; // Если фильтр 'all', возвращаем все
+    }
 
-
-    return affairs // need to fix
+    return affairs = affairs.filter(t => t.priority === filter); // need to fix
 }
 export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
 
     return affairs // need to fix
 }
 
-function HW2() {
+export function HW2() {
     const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
