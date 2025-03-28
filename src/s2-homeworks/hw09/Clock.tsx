8b1c591d2c30d9e1963afc37e787b8eb4a5a321b
Filipp Sam <philippsm666@gmail.com>
2025-03-29 01:07:54 +0300

    refactochange date and month

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -46,12 +46,8 @@ function Clock() {
   ).padStart(2, "0")}.${date.getFullYear()}` || <br />; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-  const stringDay = new Intl.DateTimeFormat("en", { weekday: "long" }).format(
-    new Date(2022, 1, 1)
-  ) || <br />; // пишут студенты
-  const stringMonth = new Intl.DateTimeFormat("en", { month: "long" }).format(
-    new Date(2022, 1, 1)
-  ) || <br />; // пишут студенты
+  const stringDay = new Intl.DateTimeFormat("en", { weekday: "long" }).format(date) || <br />; // пишут студенты
+  const stringMonth = new Intl.DateTimeFormat("en", { month: "long" }).format(date) || <br />; // пишут студенты
 
   return (
     <div className={s.clock}>
