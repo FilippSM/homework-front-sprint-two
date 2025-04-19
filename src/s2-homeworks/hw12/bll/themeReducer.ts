9d61e7168ee34ac0d2467739a16c8541f3dbcb9d
Filipp Sam <philippsm666@gmail.com>
2025-04-20 00:16:00 +0300

    (sprint 4 week 4) HW 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
+export type ThemeState = {
+  themeId: number;
+};
+
+type ThemeAction = {
+  type: "SET_THEME_ID";
+  id: number;
+};
+
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (state = initState, action: ThemeAction): ThemeState => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case 'SET_THEME_ID':
+        return { ...state, themeId: action.id }
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state
-    }
-}
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ThemeAction => ({
+  type: "SET_THEME_ID",
+  id,
+}); // fix any
